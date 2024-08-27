b0398b938d011e5b3419982f89f0b14c46ab7bcf
Chey Rose <cheyroseflammer@gmail.com>
2024-08-27 12:10:04 -0700

    feature start: 3D Model start. next: implement model with threejs utilizing gsap for animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
+import ThreeDModel from "./components/3DModel";
 
 const App = () => {
 	return (
 		<main className="bg-black">
-			<Navbar /> <Hero /> <Highlights />
+			<Navbar />
+			<Hero />
+			<Highlights />
+			<ThreeDModel />
 		</main>
 	);
 };
